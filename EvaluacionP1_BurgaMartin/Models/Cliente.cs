c686aa288bc3433c66b0e552ab5ab8d2a79efd67
laboratorio <laboratorio@UPOAULA10719>
2025-04-28 07:22:15 -0500

    Agregar archivos de proyecto.

---
+++ EvaluacionP1_BurgaMartin/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EvaluacionP1_BurgaMartin.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength]
+        public string Nombre { get; set; }
+        public bool PagoRealizado { get; set; }
+        public DateOnly FechaPago { get; set; }
+        public double Monto { get; set; }
+       
+
+    }
+}
---
+++ EvaluacionP1_BurgaMartin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EvaluacionP1_BurgaMartin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
